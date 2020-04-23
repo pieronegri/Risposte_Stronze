19e52cd8660291b63e38bb8766b340d31003b00b
pieronegri <pieronegri@gmail.com>
2020-04-23 19:40:46 +0200

    build sdk28

--- app/src/main/java/pieronegri/rispostestronze/ui/controller/MainActivity.java
+++ app/src/main/java/pieronegri/rispostestronze/ui/controller/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
                 .detectDiskReads()
                 .detectDiskWrites()
-                .detectNetwork()   // or .detectAll() for all detectable problems
+                .detectAll() //for all detectable problems
                 .penaltyLog()
                 .build());
         StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
